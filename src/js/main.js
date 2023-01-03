0a33a77a6a186eac3c2bd07d80b1931b25885a1c
Volodymyr <getskowork@gmail.com>
2023-01-03 17:43:01 +0200

    Added styles header menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import './libs/sliders';
 
 import modals from './modules/modals';
+import navigationFixation from "./modules/navigationFixation";
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
+    navigationFixation('.header', '.header__container');
 });
