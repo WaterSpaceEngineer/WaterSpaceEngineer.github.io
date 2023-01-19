e2fcbbd53daa98d4b6ffe8890e8c6e59ccec6549
Volodymyr <getskowork@gmail.com>
2023-01-19 16:53:34 +0200

    Added product page structure

--- src/js/libs/sliders.js
+++ src/js/libs/sliders.js
@@ -1,7 +1,33 @@
-const swiper = new Swiper('.swiper', {
+import Swiper, { Autoplay, Navigation, Thumbs } from "swiper";
+
+new Swiper('.hero-swiper', {
+  modules: [Autoplay],
   loop: true,
+  speed: 750,
 
   autoplay: {
     delay: 3500,
+  },
+});
+
+const galleryThumbs = new Swiper('.gallery-thumbs', {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+const galleryTop = new Swiper('.gallery-top', {
+  modules: [Navigation, Thumbs],
+  loop: true,
+  speed: 750,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: galleryThumbs,
   },
 });
