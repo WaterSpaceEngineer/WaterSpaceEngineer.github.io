0ca61be38b332876b36d88cb4980eb577aa59b95
Volodymyr <getskowork@gmail.com>
2023-02-02 12:43:27 +0200

    Created page travel-filter

--- src/js/modules/playVideo.js
+++ src/js/modules/playVideo.js
@@ -1,30 +1,30 @@
 const playVideo = (triggerSelector) => {
     const btnPlay = document.querySelector(triggerSelector);
 
-    const tag = document.createElement('script');
+    if(btnPlay) {
+        const tag = document.createElement('script');
 
-    tag.src = "https://www.youtube.com/iframe_api";
-    const firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-    let player;
-
-    function createPlayer (url) {
-        player = new YT.Player('frame', {
-            height: '100%',
-            width: '100%',
-            videoId: `${url}`,
-            playerVars: { 'autoplay': 1 },
+        tag.src = "https://www.youtube.com/iframe_api";
+        const firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    
+        let player;
+    
+        function createPlayer (url) {
+            player = new YT.Player('frame', {
+                height: '100%',
+                width: '100%',
+                videoId: `${url}`,
+                playerVars: { 'autoplay': 1 },
+            });
+        }
+    
+        btnPlay.addEventListener('click', () => {
+            const path = btnPlay.getAttribute('data-url');
+            createPlayer(path);
+            btnPlay.remove();
         });
     }
-
-    btnPlay.addEventListener('click', () => {
-        const path = btnPlay.getAttribute('data-url');
-        createPlayer(path);
-        btnPlay.remove();
-    });
-
-    
 };
 
 export default playVideo;
