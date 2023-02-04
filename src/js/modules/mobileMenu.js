7a4007769cffed8726e82c12a5c67e6309a366ef
Volodymyr <getskowork@gmail.com>
2023-02-04 12:06:12 +0200

    Added menu sub-items

--- src/js/modules/mobileMenu.js
+++ src/js/modules/mobileMenu.js
@@ -3,21 +3,21 @@ const mobileMenu = (menuBtnSelector, menuBlockSelector) => {
   const menuBlock = document.querySelector(menuBlockSelector);
   let toggleTriger = false;
 
-  menuBtn.addEventListener('click', () => {
-      if(!toggleTriger) {
-          menuBtn.classList.add('open');
-          menuBtn.classList.remove('close');
-          menuBlock.classList.add('open');
-          document.body.style.overflow = 'hidden';
-          toggleTriger = true;
-      } else {
-          menuBtn.classList.remove('open');
-          menuBtn.classList.add('close');
-          menuBlock.classList.remove('open');
-          document.body.style.overflow = '';
-          toggleTriger = false;
-      }
-  })
+  menuBtn.addEventListener("click", () => {
+    if (!toggleTriger) {
+      menuBtn.classList.add("open");
+      menuBtn.classList.remove("close");
+      menuBlock.classList.add("open");
+      document.body.style.overflow = "hidden";
+      toggleTriger = true;
+    } else {
+      menuBtn.classList.remove("open");
+      menuBtn.classList.add("close");
+      menuBlock.classList.remove("open");
+      document.body.style.overflow = "";
+      toggleTriger = false;
+    }
+  });
 }
 
 export default mobileMenu;
